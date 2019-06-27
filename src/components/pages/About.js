eb4474a20f7a95b58273f33b70c16c30a773cb5f
Sudarshan <snkoirala45@gmail.com>
2019-06-27 21:04:15 +0300

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>
+        This is the simple react TodoList. Axios and JSONPlaceholder is used to
+        generate fake online REST API for testing.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
